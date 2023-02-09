74a33a5a402af7bc04cd7703c826e4e390b890f0
bunbuntigery <53885737+bunbuntigery@users.noreply.github.com>
2023-02-08 18:01:58 -0700

    added files

---
+++ src/MyToken.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract MyToken {
+    constructor() {
+        // Create MyToken ERC-20 here
+    }
+}
---
+++ test/Pool.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+contract PoolTest is Test {
+    address uniRouterAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+
+    function setUp() public {
+        // Deploy MyToken.sol
+        // Create pool using factory address to include MyToken/WETH
+    }
+
+    function testAddLiquidity() public {
+        // Add liquidity to pool here
+    }
+
+    function testRemoveLiquidity() public {
+        // Remove liquidity to pool here
+    }
+
+    function testSwap() public {
+        // Perform a swap in the liquidity pool
+    }
+}
