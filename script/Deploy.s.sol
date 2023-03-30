ae6456de92d0366ffb448cb4e4ff9d5c666b4f71
Jacob Firek <jacob@interlay.io>
2023-03-29 22:23:50 -0400

    code

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/MyToken.sol";
 
 contract DeployScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 initialSupply = 1e28; // 1 million tokens in atomic notation
+        MyToken mytoken = new MyToken(initialSupply); // 
+        vm.broadcast(); // collects state change function calls to be executed
     }
 }
