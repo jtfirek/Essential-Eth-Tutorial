e0e135184f3449c7558cf60677bcd78ecf41abd2
Jacob Firek <jacob@interlay.io>
2023-03-29 21:03:59 -0400

    cleaning

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
-        _mint(msg.sender, initialSupply);
-    }
-}
+// contract MyToken is ERC20 {
+//     constructor(uint256 initialSupply) ERC20("MyToken", "MTK") {
+//         _mint(msg.sender, initialSupply);
+//     }
+// }
