f0d220f5234027e0872c182dc03b6a728e09b8c7
Jacob Firek <jacob@interlay.io>
2023-03-31 12:47:57 -0400

    code

--- test/Pool.t.sol
+++ test/Pool.t.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/MyToken.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "v2-core/interfaces/IUniswapV2Factory.sol"; // Uniswap V2 Factory
 import "v2-periphery/interfaces/IUniswapV2Router02.sol"; // Uniswap V2 Router
 import "v2-periphery/interfaces/IWETH.sol";
 import {Utils} from "./utils/Utils.sol";
+
+// Define the combined interface
 
 
 contract PoolTest is Test {
@@ -15,12 +18,15 @@ contract PoolTest is Test {
     Utils internal utils;
     address payable[] internal users;
     address internal me;
+
     // Uniswap V2 Router
-    address internal uniRouterAddress = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address internal UNISWAP_ROUTER_ADDRESS = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     IUniswapV2Router02 internal uniRouter;
+
     // Uniswap V2 Factory
     address internal UNISWAP_FACTORY_ADDRESS = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
     IUniswapV2Factory internal uniswapFactory;
+
     // Wrapped Ether instance
     IWETH weth;
     // MyToken instance
@@ -40,29 +46,28 @@ contract PoolTest is Test {
         /// create instance of the UniswapV2Factory interface which allows us to interact with the Uniswap V2 Factory
         uniswapFactory = IUniswapV2Factory(UNISWAP_FACTORY_ADDRESS);
 
-        // create instance of the UniswapV2Router02 interface
-        uniRouter = IUniswapV2Router02(uniRouterAddress);
+        // // create instance of the UniswapV2Router02 interface
+        uniRouter = IUniswapV2Router02(UNISWAP_ROUTER_ADDRESS);
 
-        // Create an instance of the IWETH interface which allows us to interact with the WETH contract
-        weth = IWETH(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
-        // mint 1 million tokens to the me
-        // 1e6 = 1 million, 1 ERC20 = 1e18 atomic units
-        // 1e6 * 1e18 = 1e24
+        // // Create an instance of the IWETH interface which allows us to interact with the WETH contract
+        weth = IWETH(uniRouter.WETH());
+        
+        // // mint 1 million tokens to the me
+        // // 1e6 = 1 million, 1 ERC20 = 1e18 atomic units
+        // // 1e6 * 1e18 = 1e24
         myToken.mint(me, 1e24);
-
-        // Create pool using factory address to include MyToken/WETH
+        
+        // // Create pool using factory address to include MyToken/WETH
         uniswapFactory.createPair(address(weth), myTokenAddress);
     }
 
     function test_setup() public {
-        // check balance of my account
-        address myTokenAddress = address(myToken);
-        uint256 balance = ERC20(myTokenAddress).balanceOf(me);
+        // check if the balance of the me is 1 million 
+        uint256 balance = ERC20(address(myToken)).balanceOf(me);
         assertEq(balance, 1e24, "Balance should be 1 million");
 
         // check if the pool exists
-        address pairAddress = uniswapFactory.getPair(address(weth), myTokenAddress);
+        address pairAddress = uniswapFactory.getPair(address(weth), address(myToken));
         assertTrue(pairAddress != address(0), "Pair address should not be 0");
     }
 
@@ -74,7 +79,7 @@ contract PoolTest is Test {
         // approve the router allowance of my tokens and wrap eth
         myToken.approve(address(uniRouter), 1e24);
         weth.deposit{value: 50 ether}();
-        weth.approve(address(uniRouter), 50 ether);
+        IERC20(address(weth)).approve(address(uniRouter), 50 ether);
 
         // add liquidity to pool
         uniRouter.addLiquidity(
