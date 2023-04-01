5da36c7702548e207f5eab8995c8d5aa1141cac2
Jacob Firek <jacob@interlay.io>
2023-03-31 23:33:15 -0400

    frontend start

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,21 @@
+//src/components/header.tsx
+import NextLink from "next/link"
+import { Flex, Button, useColorModeValue, Spacer, Heading, LinkBox, LinkOverlay } from '@chakra-ui/react'
+
+const siteTitle="ERC-20 Token Generator"
+export default function Header() {
+
+  return (
+    <Flex as='header' bg={useColorModeValue('gray.100', 'gray.900')} p={4} alignItems='center'>
+      <LinkBox>
+        <NextLink href={'/'} passHref>
+          <LinkOverlay>
+            <Heading size="md">{siteTitle}</Heading>
+          </LinkOverlay>
+        </NextLink>
+      </LinkBox>      
+      <Spacer />
+      <Button> Connect </Button>
+    </Flex>
+  )
+}
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,115 @@
+// src/pages/index.tsx
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import NextLink from "next/link"
+import { VStack, Heading, Box, LinkOverlay, LinkBox} from "@chakra-ui/layout"
+import { Text, Button } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react';
+import { useState, useEffect} from 'react'
+import {ethers} from "ethers"
+import ReadERC20 from "components/ReadERC20"
+import TransferERC20 from "components/TransferERC20"
+
+declare let window:any
+const addressERC20 = '0x5FbDB2315678afecb367f032d93F642f64180aa3'
+
+const Home: NextPage = () => {
+  const [balance, setBalance] = useState<string | undefined>()
+  const [currentAccount, setCurrentAccount] = useState<string | undefined>()
+  const [chainId, setChainId] = useState<number | undefined>()
+  const [chainname, setChainName] = useState<string | undefined>()
+
+  useEffect(() => {
+    //get ETH balance and network info only when having currentAccount 
+    if(!currentAccount || !ethers.utils.isAddress(currentAccount)) return
+
+    //client side code
+    if(!window.ethereum) {
+      console.log("please install MetaMask")
+      return
+    }
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.getBalance(currentAccount).then((result)=>{
+      setBalance(ethers.utils.formatEther(result))
+    }).catch((e)=>console.log(e))
+
+    provider.getNetwork().then((result)=>{
+      setChainId(result.chainId)
+      setChainName(result.name)
+    }).catch((e)=>console.log(e))
+
+  },[currentAccount])
+
+  //click connect
+  const onClickConnect = () => {
+    //client side code
+    if(!window.ethereum) {
+      console.log("please install MetaMask")
+      return
+    }
+    /*
+    //change from window.ethereum.enable() which is deprecated
+    //call window.ethereum.request() directly
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+    .then((accounts:any)=>{
+      if(accounts.length>0) setCurrentAccount(accounts[0])
+    })
+    .catch('error',console.error)
+    */
+
+    //we can do it using ethers.js
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.send("eth_requestAccounts", [])
+    .then((accounts)=>{
+      if(accounts.length>0) setCurrentAccount(accounts[0])
+    }).catch((e)=>console.log(e))
+
+  }
+
+  //click disconnect
+  const onClickDisconnect = () => {
+    console.log("onClickDisConnect")
+    setBalance(undefined)
+    setCurrentAccount(undefined)
+  }
+
+  return (
+    <>
+      <Head>
+        <title>ERC-20 Generator</title>
+      </Head>
+
+      <Heading as='h2' size='2xl' textAlign="center" my={4}>Token Creation and Uniswap Liquidity Management</Heading>          
+      <Text textAlign="center" my={4}>
+  Create your own ERC-20 tokens, wrap ETH into ERC-20s, effortlessly deploy an ERC20/ERC20 Uniswap V2 liquidity pool, and withdraw liquidity with ease
+</Text>
+
+      <VStack>
+        <Box w='100%' my={4}>
+        {currentAccount  
+          ? <Button type="button" w='100%' onClick={onClickDisconnect}>
+                Account:{currentAccount}
+            </Button>
+          : <Button type="button" w='100%' onClick={onClickConnect}>
+            Connect MetaMask 
+               <Image src="/fox.png" alt="MetaMask Icon" boxSize="24px" mr={2} />
+            
+              </Button>
+        }
+        </Box>
+        {currentAccount  
+          ?<Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+          <Heading my={4}  fontSize='xl'>Account info</Heading>
+          <Text>ETH Balance of current account: {balance}</Text>
+          <Text>Chain Info: ChainId {chainId} name {chainname}</Text>
+        </Box>
+        :<></>
+        }
+
+      </VStack>
+    </>
+  )
+}
+
+export default Home
